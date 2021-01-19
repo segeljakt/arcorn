5b629c1cd008c161a437970a9d9b3b440591c7d4
Klas Segeljakt <klasseg@kth.se>
2021-01-19 15:41:54 +0100

    Fix unused code warning

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -16,6 +16,7 @@ arcorn! {
     struct Nil {}
 }
 
+#[test]
 fn test() {
     let tail = Rc::new(List::Nil(Rc::new(Nil {})));
     let tail = Rc::new(List::Cons(Rc::new(Cons { val: 0, tail })));
@@ -24,7 +25,7 @@ fn test() {
     let tail = Rc::new(List::Cons(Rc::new(Cons { val: 4, tail })));
 
     let arcon_tail: arcon_types::List = tail.as_ref().into();
-    let arc_tail: List = arcon_tail.into();
+    let _arc_tail: List = arcon_tail.into();
 }
 
 // To view generated code:
